a91f3cd081383949675b4ca630fad763c1a9190f
Raghu K <raghuk.vit@gmail.com>
2023-02-22 06:48:12 +0530

    Update tf-module-app/

--- main.tf
+++ main.tf
@@ -24,3 +24,28 @@ resource "aws_security_group" "main" {
   )
 }
 
+resource "aws_launch_template" "main" {
+  name_prefix   = "${var.env}-${var.component}-template"
+  image_id      = data.aws_ami.centos8.id
+  instance_type = var.instance_type
+}
+
+resource "aws_autoscaling_group" "asg" {
+  name             = "${var.env}-${var.component}-asg"
+  max_size         = var.max_size
+  min_size         = var.min_size
+  desired_capacity = var.desired_capacity
+  force_delete     = true
+
+  vpc_zone_identifier = var.subnet_ids
+
+  dynamic "tag" {
+    for_each = local.all_tags
+    content {
+      key                 = each.key
+      value               = each.value
+      propagate_at_launch = true
+    }
+  }
+}
+
