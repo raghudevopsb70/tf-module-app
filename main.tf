a4687c8323714b4a7b5dbc6c0d38f081371492b2
Raghu K <raghuk.vit@gmail.com>
2023-02-22 06:56:38 +0530

    Update tf-module-app/

--- main.tf
+++ main.tf
@@ -31,13 +31,17 @@ resource "aws_launch_template" "main" {
 }
 
 resource "aws_autoscaling_group" "asg" {
-  name             = "${var.env}-${var.component}-asg"
-  max_size         = var.max_size
-  min_size         = var.min_size
-  desired_capacity = var.desired_capacity
-  force_delete     = true
-
+  name                = "${var.env}-${var.component}-asg"
+  max_size            = var.max_size
+  min_size            = var.min_size
+  desired_capacity    = var.desired_capacity
+  force_delete        = true
   vpc_zone_identifier = var.subnet_ids
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  }
 
   dynamic "tag" {
     for_each = local.all_tags
