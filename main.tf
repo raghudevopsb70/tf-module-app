cf3ce4dd97067a29089f63151aac069858cf66c4
Raghu K <raghuk.vit@gmail.com>
2023-02-22 06:16:43 +0530

    Update tf-module-app/

--- main.tf
+++ main.tf
@@ -1,22 +1,12 @@
-resource "aws_docdb_subnet_group" "default" {
-  name       = "${var.env}-docdb-subnet-group"
-  subnet_ids = var.subnet_ids
-
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-docdb-subnet-group" }
-  )
-}
-
-resource "aws_security_group" "docdb" {
-  name        = "${var.env}-docdb-security-group"
-  description = "${var.env}-docdb-security-group"
+resource "aws_security_group" "main" {
+  name        = "${var.env}-${var.component}-security-group"
+  description = "${var.env}-${var.component}-security-group"
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "MongoDB"
-    from_port   = 27017
-    to_port     = 27017
+    description = "HTTP"
+    from_port   = var.app_port
+    to_port     = var.app_port
     protocol    = "tcp"
     cidr_blocks = var.allow_cidr
   }
@@ -30,7 +20,7 @@ resource "aws_security_group" "docdb" {
 
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-docdb-security-group" }
+    { Name = "${var.env}-${var.component}-security-group" }
   )
 }
 
