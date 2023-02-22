4f68734ecfb6772a0ee9384f52737e0402263241
Raghu K <raghuk.vit@gmail.com>
2023-02-22 07:25:24 +0530

    Update tf-module-app/

--- main.tf
+++ main.tf
@@ -66,6 +66,9 @@ resource "aws_launch_template" "main" {
   image_id               = data.aws_ami.centos8.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.profile.arn
+  }
 }
 
 resource "aws_autoscaling_group" "asg" {
