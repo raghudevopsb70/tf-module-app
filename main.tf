d393ccc6483cd0e60dd95af326f4f3e5e360c54c
Raghu K <raghuk.vit@gmail.com>
2023-02-23 06:19:08 +0530

    Update tf-module-app/

--- main.tf
+++ main.tf
@@ -107,26 +107,26 @@ resource "aws_launch_template" "main" {
   }
 }
 
-resource "aws_autoscaling_group" "asg" {
-  name                = "${var.env}-${var.component}-asg"
-  max_size            = var.max_size
-  min_size            = var.min_size
-  desired_capacity    = var.desired_capacity
-  force_delete        = true
-  vpc_zone_identifier = var.subnet_ids
-
-  launch_template {
-    id      = aws_launch_template.main.id
-    version = "$Latest"
-  }
-
-  dynamic "tag" {
-    for_each = local.all_tags
-    content {
-      key                 = tag.value.key
-      value               = tag.value.value
-      propagate_at_launch = true
-    }
-  }
-}
+//resource "aws_autoscaling_group" "asg" {
+//  name                = "${var.env}-${var.component}-asg"
+//  max_size            = var.max_size
+//  min_size            = var.min_size
+//  desired_capacity    = var.desired_capacity
+//  force_delete        = true
+//  vpc_zone_identifier = var.subnet_ids
+//
+//  launch_template {
+//    id      = aws_launch_template.main.id
+//    version = "$Latest"
+//  }
+//
+//  dynamic "tag" {
+//    for_each = local.all_tags
+//    content {
+//      key                 = tag.value.key
+//      value               = tag.value.value
+//      propagate_at_launch = true
+//    }
+//  }
+//}
 
